fbcd970184f1208c8aa28d4e4c707107fb4691b3
Budhi Gustiandi <budhigustiandi@gmail.com>
2018-02-02 04:51:15 +0000

    Create variable outside the curl function in create_thing.sh part 2

--- create_thing.sh
+++ create_thing.sh
@@ -4,18 +4,19 @@ name="Temperature Monitoring System"
 description="Sensor system monitoring area temperature"
 url="https://scratchpad.sensorup.com/OGCSensorThings/v1.0/Things"
 # optional property
-property_name_1="Deployment Condition"
-property_description_1="Deployed in a third floor balcony"
-property_name_2="Case Used"
-property_description_2="Radiation Shield"
-property_name_3="Owner"
-property_description_3="Budhi Gustiandi"
-curl -XPOST -H "Content-type: application/json" -d '{
-	"name": "$name",
-	"description": "$description",
+property_name=()
+property_name+=("Deployment Condition")
+property_name+=("Case Used")
+property_name+=("Owner")
+property_description=("Deployed in a third floor balcony" "Radiation shield" "Budhi Gustiandi")
+content='{
+	"name": "Temperature Monitoring System",
+	"description": "Sensor system monitoring area temperature",
 	"properties": {
-		"$property_name_1": "$property_description_1",
-		"$property_name_2": "$property_description_2",
-		"$property_name_3": "$property_description_3"
+		"Deployment Condition": "Deployed in a third floor balcony",
+		"Case Used": "Radiation shield",
+		"Owner": "Budhi Gustiandi"
 	}
-}' "$url"
+}'
+attributes=`echo $content`
+curl -XPOST -H "Content-type: application/json" -d "$content" "$url"
