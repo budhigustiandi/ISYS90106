2c6737fbf2af41e822e6e25968dc1b28e632eb0a
Budhi Gustiandi <budhigustiandi@gmail.com>
2018-02-03 16:09:39 +0700

    Add datastream entity when create a new thing.

--- create_thing.sh
+++ create_thing.sh
@@ -6,7 +6,7 @@ echo "Create a new thing..."
 
 thing_name=`cat configuration.txt | grep thing_name | cut -d ":" -f 2`
 thing_description=`cat configuration.txt | grep thing_description | cut -d ":" -f 2`
-url="https://scratchpad.sensorup.com/OGCSensorThings/v1.0/Things"
+url=`cat configuration.txt | grep url | cut -d ":" -f 2`
 mandatory_content="\"name\": \"$thing_name\",
                    \"description\": \"$thing_description\""
 echo "Thing's name is $thing_name."
@@ -77,6 +77,60 @@ if [[ "$add_location" == True ]]; then
 	content="$content, $location"
 else
 	echo "No location added to the created thing."
+fi
+
+# datastream property
+
+add_datastream=`cat configuration.txt | grep add_datastream | cut -d ":" -f 2`
+if [[ "$add_datastream" == "True" ]]; then
+	datastream_name=`cat configuration.txt | grep datastream_name | cut -d ":" -f 2`
+	echo "Datastream name is $datastream_name."
+	datastream_description=`cat configuration.txt | grep datastream_description | cut -d ":" -f 2`
+	echo "Datastream description is $datastream_description."
+	datastream_observation_type=`cat configuration.txt | grep datastream_observation_type | cut -d ":" -f 2`
+	echo "Datastream observation type is $datastream_observation_type."
+	unit_of_measurement_name=`cat configuration.txt | grep unit_of_measurement_name | cut -d ":" -f 2`
+	echo "Unit of measurement name is $unit_of_measurement_name."
+	unit_of_measurement_symbol=`cat configuration.txt | grep unit_of_measurement_symbol | cut -d ":" -f 2`
+	echo "Unit of measurement symbol is $unit_of_measurement_symbol."
+	unit_of_measurement_definition=`cat configuration.txt | grep unit_of_measurement_definition | cut -d ":" -f 2`
+	echo "Unit of measurement definition is $unit_of_measurement_definition."
+	observed_property_name=`cat configuration.txt | grep observed_property_name | cut -d ":" -f 2`
+	echo "Observed property name is $observed_property_name."
+	observed_property_description=`cat configuration.txt | grep observed_property_description | cut -d ":" -f 2`
+	echo "Observed property description is $observed_property_description."
+	observed_property_definition=`cat configuration.txt | grep observed_property_definition | cut -d ":" -f 2`
+	echo "Observed property definition is $observed_property_definition."
+	sensor_name=`cat configuration.txt | grep sensor_name | cut -d ":" -f 2`
+	echo "Sensor name is $sensor_name."
+	sensor_description=`cat configuration.txt | grep sensor_description | cut -d ":" -f 2`
+	echo "Sensor description is $sensor_description."
+	sensor_encoding_type=`cat configuration.txt | grep sensor_encoding_type | cut -d ":" -f 2`
+	echo "Sensor encoding type is $sensor_encoding_type."
+	sensor_metadata=`cat configuration.txt | grep sensor_metadata | cut -d ":" -f 2`
+	echo "Sensor metadata is $sensor_metadata."
+	datastream="\"Datastreams\": [{
+					\"name\": \"$datastream_name\",
+					\"description\": \"$datastream_description\",
+					\"observationType\": \"$datastream_observation_type\",
+					\"unitOfMeasurement\": {
+						\"name\": \"$unit_of_measurement_name\",
+						\"symbol\": \"$unit_of_measurement_symbol\",
+						\"definition\": \"$unit_of_measurement_definition\"
+					},
+					\"ObservedProperty\": {
+						\"name\": \"$observed_property_name\",
+						\"description\": \"$observed_property_description\",
+						\"definition\": \"$observed_property_definition\"
+					},
+					\"Sensor\": {
+						\"name\": \"$sensor_name\",
+						\"description\": \"$sensor_description\",
+						\"encodingType\": \"$sensor_encoding_type\",
+						\"metadata\": \"$sensor_metadata\"
+					}
+			   }]"
+	content="$content, $datastream"
 fi
 
 content_placeholder="{$content}"
