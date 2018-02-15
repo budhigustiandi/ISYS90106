bbdb35afb02e12d0e2717f8d0db52ba8fa78cd45
Budhi Gustiandi <budhigustiandi@gmail.com>
2018-02-15 14:28:40 +0000

    Fix the position of DHT11 and light sensors and modify the read_ script

--- observation/read_light.py
+++ observation/read_light.py
@@ -3,8 +3,8 @@ from gpiozero import LightSensor # Use gpiozero library to read light
 '''
 Connect VCC pin of light sensor to 3.3V pin of Raspberry pi 3
 Connect GND pin of light sensor to GND pin of Raspberry Pi 3
-Connect OUT pin of light sensor to GPIO14 pin of Raspberry Pi 3
+Connect OUT pin of light sensor to GPIO17 pin of Raspberry Pi 3
 '''
-ldr = LightSensor(14)
+ldr = LightSensor(17)
 lightIntensity = round(ldr.value * 1023) # GPIO pin of Raspberry Pi 3 can read 1024 level of input so that measurement is scale to 1023 value
 print lightIntensity
