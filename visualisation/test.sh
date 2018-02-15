728cfd2775b2b921372a93cb637158ce204fb176
Budhi Gustiandi <budhigustiandi@gmail.com>
2018-02-15 18:31:59 +0700

    Merge create and update location function into one single script create_update_location.sh

--- visualisation/test.sh
+++ visualisation/test.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-datastream_id=1653970
-echo '<div id="room_temperature"></div>
-		<script>
-			stachart.generateChart({
-				'"'"'targetId'"'"': '"'"'room_temperature'"'"',
-			    '"'"'staBaseUrl'"'"': '"'"'http://scratchpad.sensorup.com/OGCSensorThings/v1.0'"'"',
-			    '"'"'datastreamId'"'"': '"'"''$datastream_id''"'"'
-			});
-		 </script>'
+location_name="Location Name"
+location_description="Location Description"
+location_longitude="Location Longitude"
+location_latitude="Location Latitude"
+function location {
+	location='{"name": "'$location_name'",
+			  "description": "'$location_description'",
+			  "encodingType": "application/vnd.geo+json",
+			  "location": {
+				"type": "Point",
+					"coordinates": ['$location_longitude', '$location_latitude']
+			  }}'
+	location_name=Berubah1
+	location_description=Berubah2
+}
+location
+echo $location_name
+echo $location_description
+echo ''$location''
