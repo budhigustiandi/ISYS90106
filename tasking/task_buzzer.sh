0c4257fd41acb27235f212f813b44642322106dc
Budhi Gustiandi <budhigustiandi@gmail.com>
2018-04-07 00:39:33 +0000

    Fix bug in task_buzzer.sh.

--- tasking/task_buzzer.sh
+++ tasking/task_buzzer.sh
@@ -1,72 +1,63 @@
 #!/bin/bash
 
-#############################################################
-# Configuration:                                            #
-# S pin of buzzer is connected to a GPIO07 pin of Raspberry #
-# - pin of buzzer is connected to a GND pin of Raspberry    #
-#############################################################
+#################################################################
+# Configuration:                                                #
+# Connect S pin of the buzzer to GPIO07 pin of the Raspberry Pi #
+# Connect - pin of the buzzer to GND pin of the Raspberry Pi    #
+#################################################################
 
-# Exports GPIO07 pins to userspace
+# Exports GPIO07 pin to userspace
 echo "7" > /sys/class/gpio/export
 
-# Sets pin GPIO07 as an output
+# Sets GPIO07 pin as an output
 echo "out" > /sys/class/gpio/gpio7/direction
 
 function turn_on_buzzer {
-	# Sets pin GPIO07 to high
+	# Sets GPIO07 pin to high
 	echo "1" > /sys/class/gpio/gpio7/value
 }
 function turn_off_buzzer {
-        # Sets pin GPIO07 to low
-        echo "0" > /sys/class/gpio/gpio7/value
+	# Sets GPIO07 pin to low
+	echo "0" > /sys/class/gpio/gpio7/value
 }
 
-# Ensure that it refers to the right configuration file...
 base_url=`cat ../configuration.txt | grep base_url | cut -d "=" -f 2`
+echo "Base URL: $base_url"
 datastream_id=1931626
+echo "Datastream ID: $datastream_id"
 
-# Find if there is an observation value in the datastream
+# Find if there is already an observation value in the datastream
 observation_count=`curl -X GET -H "Content-Type: application/json" "$base_url/Datastreams($datastream_id)/Observations"`
 observation_count=`echo $observation_count | cut -d "," -f 1`
 observation_count=`echo $observation_count | cut -d ":" -f 2`
+echo "Observation Count: $observation_count"
 
-# Make a default observation value if it does not exist
+# Make an observation if it does not exist
 if [[ "$observation_count" == "0" ]]; then
-	time=`date +"%Y-%m-%dT%H:%M:%S.000Z"`
-	observation_result='Please choose "On" or "Off"'
-	echo $observation_result > buzzer_observation_result
+	echo "There is no observation exist in the datastream. An observation will be created."
+	observation_time=`date +"%Y-%m-%dT%H:%M:%S.000Z"`
+	echo "Observation Time: $observation_time"
+	observation_result=Off
+	echo "Observation Result: $observation_result"
 	curl -X POST -H "Content-Type: application/json" -d "{
-	\"phenomenonTime\": \"$time\",
-	\"resultTime\": \"$time\",
-	\"result\": \"$observation_result\",
-	\"Datastream\":{\"@iot.id\":$datastream_id}
-	}" "https://scratchpad.sensorup.com/OGCSensorThings/v1.0/Observations"
+		\"phenomenonTime\": \"$observation_time\",
+		\"resultTime\": \"$observation_time\",
+		\"result\": \"$observation_result\",
+		\"Datastream\":{\"@iot.id\":$datastream_id}
+	}" "$base_url/Observations"
 fi
 
-# Read status of buzzer from a "result" value in the observation entity
-result=`curl -X GET -H "Content-Type: application/json" "$base_url/Datastreams($datastream_id)/Observations"`
-result=`echo $result | sed -e 's/^{//g' -e 's/}$//g' -e 's/"@iot.id":/\n"@iot.id":/g' -e 's/"@iot.selfLink":/\n"@iot.selfLink":/g' -e 's/"phenomenonTime":/\n"phenomenonTime":/g' -e 's/"result":/\n"result":/g' -e 's/"resultTime":/\n"resultTime":/g' -e 's/"Datastream@iot.navigationLink":/\n"Datastream@iot.navigationLink":/g' -e 's/"FeatureOfInterest@iot.navigationLink":/\n"FeatureOfInterest@iot.navigationLink":/g' | grep '"result":'`
-result=`echo $result | cut -d "\"" -f 4`
+# Read current observation result from the server
+server_observation_result=`curl -X GET -H "Content-Type: application/json" "$base_url/Datastreams($datastream_id)/Observations"`
+server_observation_result=`echo $server_observation_result | sed -e 's/"result":/\n"result":/g' | grep '"result":'`
+server_observation_result=`echo $server_observation_result | cut -d '"' -f 4`
+echo "Server Observation Result: $server_observation_result"
 
-observation_result=`cat buzzer_observation_result`
-time=`date +"%Y-%m-%dT%H:%M:%S.000Z"`
-if [[ "$result" == "On" ]]; then
+# Turn on or off buzzer based on the server_observation_result
+if [[ "$server_observation_result" == "On" ]]; then
 	turn_on_buzzer
-	new_observation_result=On
-elif [[ "$result" == "Off" ]]; then
+	echo "The buzzer is on."
+elif [[ "$server_observation_result" == "Off" ]]; then
 	turn_off_buzzer
-	new_observation_result=Off
-else
-	new_observation_result='Please choose "On" or "Off"'
-fi
-
-if [[ "$new_observation_result" != "$observation_result" ]]; then
-	# Update the result in the observation
-	curl -X PATCH -H "Content-Type: application/json" -d "{
-	\"phenomenonTime\": \"$time\",
-	\"resultTime\": \"$time\",
-	\"result\": \"$new_observation_result\",
-	\"Datastream\":{\"@iot.id\":$datastream_id}
-	}" "https://scratchpad.sensorup.com/OGCSensorThings/v1.0/Observations"
-	echo $new_observation_result > buzzer_observation_result
+	echo "The buzzer is off."
 fi
